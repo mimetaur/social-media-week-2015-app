643991f5f9ffec4c86b76978b7cb108865566440
Nathan Koch <nathanrkoch@gmail.com>
2015-10-23 15:02:53 -0500

    added action endpoint for ifttt

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/light/1/color', function(req, res) {
   res.send('r0,g255,b0\n');
 });
 
+app.post('/action', function(req, res) {
+  res.send("this sent, congratulations!");
+})
+
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
