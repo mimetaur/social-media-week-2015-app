2efd64516f8715b474923df650318f61b38f5b4b
Nathan Koch <nathanrkoch@gmail.com>
2015-10-30 14:48:41 -0500

    alternate settings

---
+++ arduino/sketch/settings-alt.h
@@ -0,0 +1,8 @@
+#define MAC_ADDRESS { 0xDE, 0xAD, 0xBE, 0xEF, 0xFF, 0xED }
+#define PUSHER_KEY "62b391c0f827198df078"
+#define LIGHTUP_DURATION 1000
+#define IS_LISTENING_FRIENDS false
+#define IS_LISTENING_ADS true
+#define RED 0
+#define GREEN 0
+#define BLUE 255
