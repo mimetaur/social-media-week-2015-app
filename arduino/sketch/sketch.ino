5ce836ad815c3c644a911f4b88259a365c3a0f85
Kent Widman <KWidman@C-K.com>
2015-11-19 15:01:34 -0600

    Added code to reset the arduino on disconnect.

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -8,31 +8,37 @@
 PusherClient client;
 byte mac[] = MAC_ADDRESS;
 
+void softReset(){
+  asm volatile ("  jmp 0");
+}
+
 void setup() {
   pinMode(8, OUTPUT);
   digitalWrite(8, HIGH);
   pinMode(9, OUTPUT);
   digitalWrite(9, HIGH);
-  
+
 	Serial.begin(9600);
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Init Ethernet failed");
   }
 
-	if(client.connect(PUSHER_KEY)) {
-		client.bind("friends_tweeted", lightUpFriends);
-		client.bind("ads_tweeted", lightUpAds);
-		client.subscribe("smw2015_channel");
-	}
-	else {
-		while(1) {}
-	}
+  Serial.println("Disconnected");
+
+  if(client.connect(PUSHER_KEY)) {
+    client.bind("friends_tweeted", lightUpFriends);
+    client.bind("ads_tweeted", lightUpAds);
+    client.subscribe("smw2015_channel");
+    Serial.println("Connected");
+  }
 }
 
 void loop() {
 	if (client.connected()) {
 		client.monitor();
-	}
+	} else {
+    softReset();
+  }
 }
 
 void lightUpFriends(String data) {
